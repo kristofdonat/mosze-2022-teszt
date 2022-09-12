5f588050d204987d49fcf03366536fa91d0bcace
kristofdonat <72193904+kristofdonat@users.noreply.github.com>
2022-09-12 23:02:15 +0200

    Hiba dokumentacio
    Hibák dokumentálva

--- main.cpp
+++ main.cpp
@@ -4,24 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // hianyzik egy _ hibas valtozo
+    std::cout << '1-100 ertekek duplazasa'   //pontosvesszo vegere, dupla ""
+    for (int i = 0;) // hiányoznak a for ciklus paraméterei
     {
-        b[i] = i * 2;
+        b[i] = i * 2; //masodik i az +1
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)  //felesleges for ciklus, rossz hatarertek
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //hianyzik pontosvesszo, nincs megadva az ertek sem, +std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //atlagnak kezdoerteket kell adni, int helyett double javasoltabb
+    for (int i = 0; i < N_ELEMENTS, i++) //vesszo helyett pontosvesszo
     {
-        atlag += b[i]
+        atlag += b[i] //hianyzik pontosvesszo
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
-    //fogalmam sincs mit csin�lok
+    
 }
