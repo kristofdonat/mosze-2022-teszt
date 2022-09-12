bbad23a34b66d5165067cccce6504d2fd9c53274
kristofdonat <72193904+kristofdonat@users.noreply.github.com>
2022-09-12 23:08:11 +0200

    Hibajavitas2.0
    lemaradt egy endline :P

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
     std::cout << "1-100 ertekek duplazasa" <<std::endl;
     for (int i = 0; i < N_ELEMENTS; i++){
         b[i] = (i+1) * 2;
-        std::cout << "Ertek:" << b[i];
+        std::cout << "Ertek:" << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     double atlag = 0;
